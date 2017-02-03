f2c7d08ce42e5cb9071717c2ba9ef0a1257acc8d
humancopy <info@humancopy.net>
2017-02-03 14:12:39 +0100

    Add the main files

---
+++ peace.js
@@ -0,0 +1,73 @@
+/**
+ * PEACE.js - http://humancopy.net/peace-js.html
+ * Peace please, humancopy.net 2015
+ *
+ */
+
+var peaceJS;
+(function() {
+
+  var script_tag = document.querySelector('script[src*="peace-js"]'),
+  auto_load      = script_tag && (script_tag.getAttribute('data-auto') === null || script_tag.getAttribute('data-auto') === 'true');
+
+  peaceJS = function(options) {
+
+    options = options || {};
+
+    // Got a jQuery object
+    if (typeof jQuery != 'undefined' && options instanceof jQuery) options = {target: options[0]};
+    // Got a DOM element
+    else if (options.nodeType) options = {target: options};
+    // Default
+    else options.target = script_tag && (script_tag.getAttribute('data-target') || script_tag.parentNode);
+
+    // possible options: text, symbol
+    options.style = options.style || script_tag.getAttribute('data-style') || 'text';
+
+    var peace      = ['Peace', 'Paz', 'שלום', 'سلام', '平和', 'शांति', 'Paix', 'мир', 'Pace', 'Frieden'],
+    target_element = options.target || document.getElementsByTagName('footer')[0] || document.body;
+
+    // Do the do if can do
+    if (target_element) {
+      // Load the CSS
+      link       = document.createElement('link');
+      link.href  = 'https://humancopy.s3.amazonaws.com/peace-js/peace.min.css';
+      link.type  = 'text/css';
+      link.rel   = 'stylesheet';
+      link.media = 'screen,print';
+      document.getElementsByTagName('head')[0].appendChild(link);
+
+      // Create the peace containing paragraph & a text node
+      var paragraph = document.createElement('P'),
+      link_node     = document.createElement('A'),
+      span_node     = document.createElement('SPAN');
+
+      paragraph.className = 'peace ' + options.style;
+
+      // Append & set the link element
+      paragraph.appendChild(link_node);
+      link_node.href = 'http://humancopy.net/peace-js.html';
+      link_node.appendChild(document.createTextNode(peace.join(', ')));
+
+      // Do we want to show the peace symbol?
+      if (options.style == 'symbol') {
+        // Append peace symbol element
+        paragraph.appendChild(span_node);
+
+        // onclick should show the link element
+        span_node.onclick = function() {
+          this.parentNode.className = this.parentNode.className + ' show';
+        };
+
+        // Append the peace symbol
+        span_node.appendChild(document.createTextNode('☮'));
+      }
+
+      // Append it to target
+      target_element.appendChild(paragraph);
+    }
+  };
+
+  if (auto_load) peaceJS();
+})();
+
