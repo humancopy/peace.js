55ac0baf9a6ca58c15bf7c9591acb220b39945a9
humancopy <info@humancopy.net>
2017-02-07 23:31:58 +0100

    Upgrade to 1.1.4

--- src/peace.js
+++ src/peace.js
@@ -1,7 +1,7 @@
 /**
  * PEACE.js - http://humancopy.net/peace-js.html
  * Peace please, humancopy.net 2017
- * Version 1.1.3
+ * Version 1.1.4
  *
  */
 
@@ -38,7 +38,7 @@ var peaceJS;
     if (target_element) {
       // Load the CSS
       link       = document.createElement('link');
-      link.href  = 'https://cdn.jsdelivr.net/peace.js/1.1.3/peace.min.css';
+      link.href  = 'https://cdn.jsdelivr.net/peace.js/1.1.4/peace.min.css';
       link.type  = 'text/css';
       link.rel   = 'stylesheet';
       link.media = 'screen,print';
